5aab2acc4c3922fcb13f6ac206cc7b4c1435b94c
@nain333 <hnain70@gmail.com>
2023-03-31 23:46:10 +0530

    Hello  World!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+var name="Himanshu Nain"
 
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="name">{name}</h1>
+        
+        
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          <p className="helloWorld">
+            
+          <h1>Hello World!
+            <br></br>
+            {new Date().toDateString()}
+          </h1>
+          {console.log("Hello World!")}
+      
+          </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +33,7 @@ function App() {
         </a>
       </header>
     </div>
+    </React.Fragment>
   );
 }
 
